2980cda5d00b5dea2c82a7858ac76a260e3d677d
Pasinee Mosby <mpasinee@outlook.com>
2021-03-16 02:27:14 -0400

    second commit

---
+++ HelloWorld/src/main/java/com/pm1st/hello_world/Hello.java
@@ -0,0 +1,16 @@
+/**
+ *@author pasin
+ *email: mpasinee@outlook.com
+ *date: mar 13,2021
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.pm1st.hello_world;
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Pasinee!");
+    }
+}
